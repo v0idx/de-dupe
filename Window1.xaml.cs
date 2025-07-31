1a71f05973f950959ce5de4ec15cd18f30e6076d
v0idx <e.nash@v0idx.com>
2025-07-31 14:44:33 +0100

    Tidied up imports

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
@@ -35,7 +23,7 @@ namespace WpfApp1
         {
             if (lstMatchingFiles.SelectedItem != null)
             {
-                string txt = lstMatchingFiles.SelectedItem.ToString();
+                string? txt = lstMatchingFiles.SelectedItem.ToString();
 
                 string argument = "/select, \"" + txt + "\"";
 
